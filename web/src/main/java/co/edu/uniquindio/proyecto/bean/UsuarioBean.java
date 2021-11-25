bde8727eee2c3912a23a96269e095b4da49232af
Andres Mejia <andress.mejiaf@uqvirtual.edu.co>
2021-11-25 13:27:24 -0500

    Se hacen los combos de la ciudad en los usuarios

--- web/src/main/java/co/edu/uniquindio/proyecto/bean/UsuarioBean.java
+++ web/src/main/java/co/edu/uniquindio/proyecto/bean/UsuarioBean.java
@@ -1,6 +1,8 @@
 package co.edu.uniquindio.proyecto.bean;
 
+import co.edu.uniquindio.proyecto.entidades.Ciudad;
 import co.edu.uniquindio.proyecto.entidades.Usuario;
+import co.edu.uniquindio.proyecto.servicios.CiudadServicio;
 import co.edu.uniquindio.proyecto.servicios.UsuarioServicio;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,6 +14,7 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import java.io.Serializable;
+import java.util.List;
 
 @Component
 @ViewScoped
@@ -19,12 +22,23 @@ public class UsuarioBean implements Serializable {
 
     @Getter @Setter
     private Usuario usuario;
-    @Autowired
-    private UsuarioServicio usuarioServicio;
+    private final UsuarioServicio usuarioServicio;
+
+    private final CiudadServicio ciudadServicio;
+    @Getter @Setter
+    private Ciudad ciudad;
+    @Getter @Setter
+    private List<Ciudad> ciudades;
+
+    public UsuarioBean(UsuarioServicio usuarioServicio, CiudadServicio ciudadServicio) {
+        this.usuarioServicio = usuarioServicio;
+        this.ciudadServicio = ciudadServicio;
+    }
 
     @PostConstruct
     public void inicializar(){
         usuario= new Usuario();
+        ciudades= ciudadServicio.listarCiudades();
     }
 
     public  void  registrarUsuario(){
