3517e42efff9d987e73643b3147538d5936024ae
Andres Mejia <andress.mejiaf@uqvirtual.edu.co>
2021-12-03 17:48:17 -0500

    Listar favoritos en perfil

--- web/src/main/java/co/edu/uniquindio/proyecto/bean/SeguridadBean.java
+++ web/src/main/java/co/edu/uniquindio/proyecto/bean/SeguridadBean.java
@@ -66,16 +66,20 @@ public class SeguridadBean implements Serializable {
 
     @Getter @Setter
     private Producto productAuxiliar;
+    @Getter@Setter
+    private String telefono;
 
     @PostConstruct
     public  void inicializar(){
         this.subTotal= 0F;
         this.productosCarrito= new ArrayList<>();
+
         mediosPago= new ArrayList<>();
         mediosPago.add("PSE");
         mediosPago.add("Tarjeta credito");
         mediosPago.add("Tarjeta debito");
         mediosPago.add("Pago contraentrega");
+
     }
 
     public String inciarSesion(){
@@ -83,7 +87,7 @@ public class SeguridadBean implements Serializable {
             try {
 
                 usuarioSesion= usuarioServicio.iniciarSesion(email, password);
-
+                telefono=usuarioSesion.getTelefonos().get(0);
                 autenticado=true;
                 return "/index?faces-redirect=true";
             } catch (Exception e) {
